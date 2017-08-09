26e90eb1b72502c511dd68b99dc4a5be5f0f7989
arsimr16 <arsimr16@g.holycross.edu>
2017-08-09 08:57:15 -0700

    add working show new tweets button

--- myScript.js
+++ myScript.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+  //var $body = $('body');
+  //$body.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+    $tweet.addClass("tweet");
+    $tweet.appendTo($('.feed'));
     index -= 1;
   }
+
+  $('.refresh').on('click', function() {
+    for(var i = 0; i < streams.home.length - 1; i++) {
+      var tweet = streams.home[i];
+      var $tweet = $('<div></div>');
+      $tweet.addClass("tweet");
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($('.feed'));
+    }
+    console.log($(this).text());
+  });
 
 });
