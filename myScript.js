31e85e0857770ef510831aa4ccbaf181ffa575b0
arsimr16 <arsimr16@g.holycross.edu>
2017-08-09 09:42:05 -0700

    add moment.js library for human-friendly time stamps

--- myScript.js
+++ myScript.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at);
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment().calendar());
     $tweet.addClass("tweet");
     $tweet.appendTo($('.feed'));
     index -= 1;
@@ -17,7 +17,7 @@ $(document).ready(function(){
       var tweet = streams.home[i];
       var $tweet = $('<div></div>');
       $tweet.addClass("tweet");
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at);
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment().calendar());
       $tweet.appendTo($('.feed'));
     }
     console.log($(this).text());
