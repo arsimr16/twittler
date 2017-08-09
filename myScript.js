a892df07d58c80e4d3d4853e1684ddd74e28a45f
arsimr16 <arsimr16@g.holycross.edu>
2017-08-09 13:29:45 -0700

    separate tweets into user, time, and text; add basic css

--- myScript.js
+++ myScript.js
@@ -1,24 +1,41 @@
 $(document).ready(function(){
-  //var $body = $('body');
-  //$body.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment().calendar());
-    $tweet.addClass("tweet");
-    $tweet.appendTo($('.feed'));
+    var $tweet = $('<div class="tweet"></div>');
+    var $user = $('<p class="user" id="' + tweet.user + '"></p>');
+    var $time = $('<p class="time"></p>');
+    var $text = $('<p class="text"></p>');
+
+    $user.text('@' + tweet.user + ':');
+    $time.text(moment().calendar());
+    $text.text(tweet.message);
+
+    $('.feed').append($tweet);
+    $tweet.append($user);
+    $tweet.append($time);
+    $tweet.append($text);
+
     index -= 1;
   }
 
   $('.refresh').on('click', function() {
     for(var i = 0; i < streams.home.length - 1; i++) {
       var tweet = streams.home[i];
-      var $tweet = $('<div></div>');
-      $tweet.addClass("tweet");
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment().calendar());
-      $tweet.appendTo($('.feed'));
+      var $tweet = $('<div class="tweet"></div>');
+      var $user = $('<p class="user" id="' + tweet.user + '"></p>');
+      var $time = $('<p class="time"></p>');
+      var $text = $('<p class="text"></p>');
+
+      $user.text('@' + tweet.user);
+      $time.text(moment().calendar());
+      $text.text(tweet.message);
+
+      $('.feed').append($tweet);
+      $tweet.append($user);
+      $tweet.append($time);
+      $tweet.append($text);
     }
     console.log($(this).text());
   });
